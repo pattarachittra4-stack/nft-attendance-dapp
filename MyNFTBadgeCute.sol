74b65ac044a527e5d386d8541e1a67f6c48f3a30
pattarachittra4-stack <pattarachittra4@gmail.com>
2026-02-28 00:40:32 +0700

    Add files via upload

---
+++ MyNFTBadgeCute.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.9/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.9/contracts/access/Ownable.sol";
+contract MyNFTBadgeCute is ERC1155, Ownable {
+    string public name = "MY NFT – Attendance Badge (Cute Minimal)";
+    string public symbol = "MYNFT-C";
+    uint256 public constant BADGE = 1;
+    constructor() ERC1155("ipfs://REPLACE_WITH_CUTE_METADATA_CID") {}
+    function mintBadge() external { _mint(msg.sender, BADGE, 1, ""); }
+    function setURI(string memory newuri) external onlyOwner { _setURI(newuri); }
+}
